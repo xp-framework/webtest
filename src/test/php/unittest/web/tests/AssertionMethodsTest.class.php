c60e7626be955232a8967e0a081d2ada7882a6cb
Timm Friebe <thekid@thekid.de>
2015-08-06 10:55:49 +0200

    Test other assertion methods

--- src/test/php/unittest/web/tests/AssertionMethodsTest.class.php
+++ src/test/php/unittest/web/tests/AssertionMethodsTest.class.php
@@ -6,6 +6,7 @@ use peer\http\HttpConnection;
 use peer\http\HttpResponse;
 use peer\http\HttpConstants;
 use peer\URL;
+use text\regex\Pattern;
 
 class AssertionMethodsTest extends \unittest\TestCase {
   private $fixture;
@@ -68,8 +69,64 @@ class AssertionMethodsTest extends \unittest\TestCase {
   }
 
   #[@test]
+  public function assertElementPresent() {
+    $this->fixture->respondWith(HttpConstants::STATUS_OK, [], '<html><body><div id="content"/></html>');
+    $this->fixture->beginAt('/');
+    $this->fixture->assertElementPresent('content');
+  }
+
+  #[@test]
+  public function assertTextPresent() {
+    $this->fixture->respondWith(HttpConstants::STATUS_OK, [], '<html><body>Content</html>');
+    $this->fixture->beginAt('/');
+    $this->fixture->assertTextPresent('Content');
+  }
+
+  #[@test]
+  public function assertTextPatternPresent() {
+    $this->fixture->respondWith(HttpConstants::STATUS_OK, [], '<html><body>Content</html>');
+    $this->fixture->beginAt('/');
+    $this->fixture->assertTextPatternPresent(new Pattern('content', Pattern::CASE_INSENSITIVE));
+  }
+
+  #[@test]
+  public function assertImagePresent() {
+    $this->fixture->respondWith(HttpConstants::STATUS_OK, [], '<html><body><img src="blank.gif"/></html>');
+    $this->fixture->beginAt('/');
+    $this->fixture->assertImagePresent('blank.gif');
+  }
+
+  #[@test]
+  public function assertLinkPresent() {
+    $this->fixture->respondWith(HttpConstants::STATUS_OK, [], '<html><body><a href="http://example.com/">.</a></html>');
+    $this->fixture->beginAt('/');
+    $this->fixture->assertLinkPresent('http://example.com/');
+  }
+
+  #[@test]
+  public function assertLinkPresentWithText() {
+    $this->fixture->respondWith(HttpConstants::STATUS_OK, [], '<html><body><a href="http://example.com/">Example</a></html>');
+    $this->fixture->beginAt('/');
+    $this->fixture->assertLinkPresentWithText('Example');
+  }
+
+  #[@test]
+  public function assertFormPresent_without_name() {
+    $this->fixture->respondWith(HttpConstants::STATUS_OK, [], '<html><body><form></form></html>');
+    $this->fixture->beginAt('/');
+    $this->fixture->assertFormPresent();
+  }
+
+  #[@test]
+  public function assertFormPresent_with_name() {
+    $this->fixture->respondWith(HttpConstants::STATUS_OK, [], '<html><body><form name="login"></form></html>');
+    $this->fixture->beginAt('/');
+    $this->fixture->assertFormPresent('login');
+  }
+
+  #[@test]
   public function assertTitleEquals() {
-    $this->fixture->respondWith(HttpConstants::STATUS_OK, [], '<html><head><title>Test</title></heady><body/></html>');
+    $this->fixture->respondWith(HttpConstants::STATUS_OK, [], '<html><head><title>Test</title></head><body/></html>');
     $this->fixture->beginAt('/');
     $this->fixture->assertTitleEquals('Test');
   }
